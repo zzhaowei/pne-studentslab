48a5054e6d5969cedff67998ea24964b0d62acda
zzhaowei <z.zhu1.2025@2025.urjc.es>
2026-03-11 18:24:45 +0100

    seqserver

--- P03/Seqserver.py
+++ P03/Seqserver.py
@@ -12,18 +12,15 @@ PORT = 8080
 IP = "127.0.0.1" # this IP address is local, so only requests from the same machine are possible
 
 
-# -- Step 1: create the socket
 ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ls.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-# -- Step 2: Bind the socket to server's IP and PORT
 ls.bind((IP, PORT))
 
-# -- Step 3: Configure the socket for listening
 ls.listen()
 
 
-print("The server is configured!")
+print("The Seq server is configured!")
 
 while True:
     print("Waiting for Clients to connect")
@@ -34,17 +31,13 @@ while True:
 
 
 
-    # -- Server stopped manually
     except KeyboardInterrupt:
         print("Server stopped by the user")
 
-        # -- Close the listenning socket
         ls.close()
 
-        # -- Exit!
         exit()
 
-    # -- Execute this part if there are no errors
     else:
 
         print("A client has connected to the server!")
@@ -52,15 +45,10 @@ while True:
 
 
 
-        # -- Read the message from the client
-        # -- The received message is in raw bytes
         msg_raw = cs.recv(2048)
 
-        # -- We decode it for converting it
-        # -- into a human-redeable string
         msg = msg_raw.decode()
 
-        # -- Print the received message
         print(f"Command received: {msg}")
 
         response = ""
